247e2929a58a3d7d86f99746a6ed5205115f4916
Marc Müller <mmueller@4tecture.ch>
2015-09-29 16:00:49 +0200

    added fakes demo

---
+++ FakesDemo/CountdownLibrary.Tests.Unit/CountdownTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+
+namespace CountdownLibrary.Tests.Unit
+{
+    [TestClass]
+    public class CountdownTests
+    {
+        [TestMethod]
+        public void CalculateCountdown_CorrectTimespan_WontWork()
+        {
+            // Arrange
+            var target = new DateTime(2015, 09, 30, 19, 0, 0, DateTimeKind.Utc);
+
+            // Act
+            var countdown = Countdown.Calculate(target);
+
+            // Assert
+            Assert.AreEqual(new TimeSpan(2, 5, 0), countdown); // won't work
+        }
+
+        [TestMethod]
+        public void CalculateCountdown_CorrectTimespan_Fakes()
+        {
+            using (ShimsContext.Create())
+            {
+                // Arrange
+                var target = new DateTime(2015, 09, 30, 19, 0, 0, DateTimeKind.Utc);
+
+                // Act
+                System.Fakes.ShimDateTime.UtcNowGet = () => new DateTime(2015, 09, 30, 16, 55, 0, DateTimeKind.Utc);
+                var countdown = Countdown.Calculate(target);
+
+                // Assert
+                Assert.AreEqual(new TimeSpan(2, 5, 0), countdown);
+            }
+        }
+    }
+}
---
+++ FakesDemo/CountdownLibrary/Countdown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownLibrary
+{
+    public class Countdown
+    {
+        public static TimeSpan Calculate(DateTime targetEventDateTime)
+        {
+            return targetEventDateTime.ToUniversalTime() - DateTime.UtcNow;
+        }
+    }
+}
