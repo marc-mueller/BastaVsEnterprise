9cd3335a28a3a5085b6b856de8a32570e9302f02
Marc Müller <mmueller@4tecture.ch>
2015-09-30 10:23:57 +0200

    added layer diagram demo

---
+++ LayerDiagramsDemo/DataAccess/MyDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using Frontend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class MyDataAccess
+    {
+        public MyDataAccess()
+        {
+            var frontend = new MyFrontend();
+        }
+    }
+}
---
+++ LayerDiagramsDemo/Frontend/MyFrontend.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class MyFrontend
+    {
+        public MyFrontend()
+        {
+            var bl = new MyBusinessLogic();
+        }
+    }
+}
